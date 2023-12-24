51b08325311e1dcc631ce344d8f73ed6b34adbc9
ShivanshuKumarPrajapati <shivanshu.rkt@gmail.com>
2023-12-24 08:42:21 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Layout from './Layout';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <Layout>
+        </Layout>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+			<div className="navbar">
+				<span className="navbar__title">Pokedex</span>
+			</div>
+		);
+}
+
+export default Header
